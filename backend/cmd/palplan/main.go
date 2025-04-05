f4ec9bc54c4af21164015c94928beed04f7f88e6
Raymond Lam <53181007+Office-Stapler@users.noreply.github.com>
2025-04-06 02:04:33 +1100

    Setup gin

--- backend/cmd/palplan/main.go
+++ backend/cmd/palplan/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 
 	"github.com/Office-Stapler/Palplan/backend/srv/config"
 	"github.com/Office-Stapler/Palplan/backend/srv/db"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -16,13 +18,21 @@ func main() {
 	if err != nil {
 		log.Fatal("Cannot load env config:", err)
 	}
+	log.Println("Config:", config)
 
 	db, err := db.NewDB(context.Background(), config)
 	if err != nil {
 		log.Fatal("Error connecting to DB.")
 	}
+	log.Println("Database: ", db)
 	defer db.Close()
 
-	log.Println("Database: ", db)
-	log.Println("Config:", config)
+	r := gin.Default()
+	log.Printf("Attempting to start Gin server with port: %d\n", config.Port)
+
+	err = r.Run(fmt.Sprintf(":%d", config.Port))
+	if err != nil {
+		panic("[Error] Gin server failed to start")
+	}
+
 }
--- backend/srv/config/config.go
+++ backend/srv/config/config.go
@@ -3,16 +3,18 @@ package config
 import "github.com/spf13/viper"
 
 type Config struct {
-	DBSource   string `mapstructure:"DB_SOURCE"`
-	ServerPort string `mapstructure:"SERVER_PORT"`
+	DBSource string `mapstructure:"DB_SOURCE"`
+	Port     int64  `mapstructure:"PORT"`
 }
 
 // LoadConfig reads configuration from file or environment variables.
 func LoadConfig(path string) (config *Config, err error) {
+	viper.SetDefault("PORT", "8080")
+	viper.SetDefault("DB_SOURCE", "/")
+
 	viper.AddConfigPath(path)
 	viper.SetConfigName("palplan")
 	viper.SetConfigType("env")
-
 	viper.AutomaticEnv()
 
 	err = viper.ReadInConfig()
