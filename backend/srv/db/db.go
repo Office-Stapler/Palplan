c821b541866738feca2d8b5a0df304cd4606ba7f
Raymond Lam <53181007+Office-Stapler@users.noreply.github.com>
2025-04-06 01:47:48 +1100

    Add query functions

--- backend/srv/db/db.go
+++ backend/srv/db/db.go
@@ -3,6 +3,7 @@ package db
 import (
 	"context"
 	"fmt"
+	"log"
 
 	"github.com/Office-Stapler/Palplan/backend/srv/config"
 	"github.com/jackc/pgx/v5/pgxpool"
@@ -38,6 +39,32 @@ func (db *DB) Close() {
 	if db.Pool != nil {
 		db.Pool.Close()
 	}
+}
+
+// Exec executes a SQL command without returning rows.
+func (db *DB) Exec(ctx context.Context, sql string, arguments ...any) error {
+	if db.Pool == nil {
+		return fmt.Errorf("database pool is nil")
+	}
+	_, err := db.Pool.Exec(ctx, sql, arguments...)
+	return err
+}
+
+// Query executes a SQL query that returns rows.
+func (db *DB) Query(ctx context.Context, sql string, arguments ...any) (pgxpool.Rows, error) {
+	if db.Pool == nil {
+		return nil, fmt.Errorf("database pool is nil")
+	}
+	return db.Pool.Query(ctx, sql, arguments...)
+}
+
+// QueryRow executes a SQL query that returns at most one row.
+func (db *DB) QueryRow(ctx context.Context, sql string, arguments ...any) pgxpool.Row {
+	if db.Pool == nil {
+		log.Println("database pool is nil") //Log instead of error, since this function returns a row.
+		return nil
+	}
+	return db.Pool.QueryRow(ctx, sql, arguments...)
 }
 
 // GetPool returns the pgxpool.Pool.
